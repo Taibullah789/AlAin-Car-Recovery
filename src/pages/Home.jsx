7902e9fd26a23089d64c660acacb905c4590addd
Taibullah789 <taib.khan323@gmail.com>
2025-11-29 07:08:57 +0500

    project build production

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ export default function Home() {
         <title>Alain Car Recovery | 24/7 Towing & Roadside Assistance</title>
         <meta 
           name="description" 
-          content="Alain Car Recovery provides fast, reliable 24/7 emergency towing and roadside assistance across Dubai. Expert team ensures your vehicle is safely recovered." 
+          content="Alain Car Recovery provides fast, reliable 24/7 emergency towing and roadside assistance across Alain. Expert team ensures your vehicle is safely recovered." 
         />
       </Helmet>
 
