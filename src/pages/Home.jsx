0418d75fa40967ec02ae717567844db11a024681
Taibullah789 <taib.khan323@gmail.com>
2025-12-26 23:12:26 +0500

    latest new changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <div>
       <Helmet>
-        <title>Al Ain Car Towing | 24/7 Towing & Roadside Assistance</title>
+        <title>Al Ain Car Towing | 24/7 Towing & Roadside Assistance 0504054022</title>
         <meta 
           name="description" 
           content="Al Ain Car Towing provides fast, reliable 24/7 emergency towing and roadside assistance across Al Ain, UAE. Expert team ensures your vehicle is safely recovered." 
